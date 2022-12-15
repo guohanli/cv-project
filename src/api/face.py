74cda58bf793647fe5b6b29738d63d30a8d0de5d
liguohan <2282224584@qq.com>
2022-12-15 21:47:54 +0800

    update api

--- src/api/face.py
+++ src/api/face.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask import jsonify, request
 
-from utils import img_url2path, img_path2name
+from utils import img_url2path, img_path2name, img_path2url, img_name2path
 
 face_api = Blueprint('face_api', __name__)
 
@@ -32,5 +32,7 @@ def get_animate():
     img_path = img_url2path(img_url)
     img_name = img_path2name(img_path)
     # todo 王婧馨，获取生成的动画图片的路径
-    img_animate_path = None
-    return jsonify({'src': img_animate_path, 'id': img_name})
+    img_animate_path = '/Users/lgh/PycharmProjects/cv-project/resource/album/dog.jpeg'
+    img_animate_name = img_name2path(img_animate_path)
+    img_animate_url = img_path2url(img_animate_path)
+    return jsonify({'src': img_animate_url, 'id': img_animate_name})
