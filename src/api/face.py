2e180ed6d1e31f2ca48172e3ba10ad10b573e931
liguohan <2282224584@qq.com>
2022-12-17 01:07:44 +0800

    fix bug

--- src/api/face.py
+++ src/api/face.py
@@ -1,11 +1,10 @@
 import cv2
+from facenet_pytorch import MTCNN, InceptionResnetV1
 from flask import Blueprint
 from flask import jsonify, request
 
-from model.recognition.recognition import match_faces,load_known_faces
 from model.recognition.filter import cartoon
-import torch
-from facenet_pytorch import MTCNN, InceptionResnetV1, extract_face
+from model.recognition.recognition import match_faces, load_known_faces
 from utils import *
 
 face_api = Blueprint('face_api', __name__)
@@ -50,24 +49,24 @@ def get_face_covers():
     people = generate_people_json_file(url_list, count)
     with open(people_json_path, 'r', encoding='utf8') as fp:
         people_data = json.load(fp)
-    #返回一个类别的一张图片和id以及count
-    for i in range(1,c):
-        url_path=get_peopleimg_path_list_for_certain_category(i)
-        #print(url_path[0],i,len(get_peopleimg_path_list_for_certain_category(i)))
-        list = []
+    # 返回一个类别的一张图片和id以及count
+    for i in range(1, c):
+        url_path = get_peopleimg_path_list_for_certain_category(i)
+        # print(url_path[0],i,len(get_peopleimg_path_list_for_certain_category(i)))
+        result_list = []
         for i in range(c - 1):
-            list.append([])
+            result_list.append([])
 
         for i in range(1, c):
             url_path = get_peopleimg_path_list_for_certain_category(i)
-            list[i - 1].append(url_path[0])
-            list[i - 1].append(i)
-            list[i - 1].append(len(get_peopleimg_path_list_for_certain_category(i)))
+            result_list[i - 1].append(url_path[0])
+            result_list[i - 1].append(i)
+            result_list[i - 1].append(len(get_peopleimg_path_list_for_certain_category(i)))
             print(url_path[0], i, len(get_peopleimg_path_list_for_certain_category(i)))
-        print(list)
-    return jsonify(list)
-    #return jsonify([{'src': url_path[0], 'face_category_id':i, 'count':len(get_peopleimg_path_list_for_certain_category(i))}])
-
+        print(result_list)
+        result_list = list(map(lambda x: {'src': x[0], 'url_path': x[1], 'count': x[2]}, result_list))
+    return jsonify(result_list)
+    # return jsonify([{'src': url_path[0], 'face_category_id':i, 'count':len(get_peopleimg_path_list_for_certain_category(i))}])
 
 
 @face_api.route('/get_somebody_faces')
@@ -76,6 +75,7 @@ def get_somebody_faces():
     # help(MTCNN)
     # help(InceptionResnetV1)
     # 获取设备
+    face_category_id = request.args.get('face_category_id')
     album_path = os.path.join(current_path, '..', 'resource', 'album')
     people_json_path = os.path.join(album_path, 'people.json')
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -122,7 +122,7 @@ def get_somebody_faces():
             list1[i - 1].append(url_path[j])
             list1[i - 1].append(name)
             # print(url_path,name)
-    #print(list1)
+        # print(list1)
         return jsonify(list1)
 
 
@@ -134,7 +134,7 @@ def get_animate():
     # todo 王婧馨，获取生成的动画图片的路径
     img_path = os.path.join(album_path, 'man1.png')
     animate_path = os.path.join(current_path, '..', 'resource')
-    save_path= os.path.join(animate_path, 'animate_pic','animate_man1.png')
+    save_path = os.path.join(animate_path, 'animate_pic', 'animate_man1.png')
     print(img_path)
     img = cv2.imread(img_path)
     # img=cv2.imread('E:/cv-project/resource/album/man1.png')
