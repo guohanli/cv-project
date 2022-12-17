2b9c08297c7214049898b58de04661dec2a77157
王婧馨 <8777260+wjx990715@user.noreply.gitee.com>
2022-12-17 13:29:44 +0800

    new_image

--- src/model/recognition/run.py
+++ src/model/recognition/run.py
@@ -16,7 +16,7 @@ def generate_people_json():
     MatchThreshold = 1  # 人脸特征向量匹配阈值设置
     a = get_people_img_path_list()  # people list
     known_faces_emb, _ = load_known_faces(a[0], mtcnn, resnet)  # 已知人物图
-    c = 1
+    label = 1
     count = [1]
     for i in range(len(a) - 1):
         known_faces_emb, _ = load_known_faces(a[i + 1], mtcnn, resnet)  # 已知人物图
@@ -27,16 +27,21 @@ def generate_people_json():
                 count.append(count[j])
             else:
                 continue
-        c = c + 1
-        count.append(c)  # 总类别是c-1
-    count.pop()
+        label = label + 1
+        count.append(label)  # 总类别是c-1
+    #count.pop()
     name = img_path_list_people_name_list(a)
     url_list = img_path_list2url_list(name)  # 'http://127.0.0.1:5000/angelababy1.png'
     # 创建一个只有people的json文件
     generate_people_json_file(url_list, count)
-
+    return count,a,label
 
 def handle_new_people_img(new_img_path):
+    mtcnn = MTCNN(min_face_size=12, thresholds=[0.2, 0.2, 0.3], keep_all=True, device=device)
+    # InceptionResnetV1模型加载【用于获取人脸特征向量】
+    resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+    MatchThreshold = 1  # 人脸特征向量匹配阈值设置
+    count,a,lable=generate_people_json()
     with open(people_json_path, 'r', encoding='utf8') as fp:
         people_data = json.load(fp)
     img_url_list, id_list = zip(*people_data)
@@ -44,8 +49,18 @@ def handle_new_people_img(new_img_path):
     # c是最新的类别id，在没有相似图片的情况下直接给新图片用
     c = len(set(id_list)) + 2
     # todo 王婧馨，将新图片和之前的图片比较，得到id，赋值给new_img_category
-    pass
-    new_img_category = None
+    for i in range(len(id_list)):
+        known_faces_emb, _ = load_known_faces(a[i], mtcnn, resnet)  # 已知人物图
+        faces_emb, img = load_known_faces(new_img_path, mtcnn, resnet)#待测任务图
+        isExistDst = match_faces(faces_emb, known_faces_emb, MatchThreshold)  # 人脸匹配
+        if isExistDst:
+            count.append(count[i])
+        else:
+            continue
+    lable=lable+1
+    count.append(lable)
+    # pass
+    new_img_category = count[-1]
     new_img_url = img_path2url(new_img_path)
     img_url_list.insert(0, new_img_url)
     id_list.insert(0, new_img_category)
